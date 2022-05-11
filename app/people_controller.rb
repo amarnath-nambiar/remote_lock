7d5d1a3a7ff6cd76066ed29ee433e0a280196538
joshi <amarnath.j@rently.com>
2022-05-11 21:16:05 +0530

    understand data formates

--- app/people_controller.rb
+++ app/people_controller.rb
@@ -4,6 +4,36 @@ class PeopleController
   end
 
   def normalize
+    puts"-------------------"
+    puts @params
+    puts @params.keys.count
+    params.each_with_index do |file,idx|
+      puts idx
+      rows = file[1].split("\n")
+      puts"_____________________"
+      puts rows
+      puts"_____________________"
+      case file[0]
+      when :dollar_format
+        rows.each do |row|
+          items = row.split(" $ ")
+          puts "................................."
+          puts items
+        end
+      # data = rows[0].split("&")
+      #
+      when :percent_format
+        rows.each do |row|
+          items = row.split(" % ")
+          puts ":::::::::::::::::::::::::::::::::"
+          puts items
+        end
+      # p data
+      end
+      puts"************************************"
+    end
+    puts"-------------------"
+
   end
 
   private
